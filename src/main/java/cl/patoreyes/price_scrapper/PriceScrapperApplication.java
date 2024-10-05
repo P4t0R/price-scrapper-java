d13e2188a47fb26b923ddc76fcc3c6793db35e50
P4t0R <patovcc@gmail.com>
2024-10-05 19:30:41 -0300

    first commit

---
+++ src/main/java/cl/patoreyes/price_scrapper/PriceScrapperApplication.java
@@ -0,0 +1,13 @@
+package cl.patoreyes.price_scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceScrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/patoreyes/price_scrapper/PriceScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.patoreyes.price_scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
